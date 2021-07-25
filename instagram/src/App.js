02ea3decea025476ce48f87c14320cff4914e60c
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-07-26 00:38:56 +0530

    added upto map on posts

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,16 +1,29 @@
 import Post from './Components/Post';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+
+  let [posts,setPosts] = useState([
+    { username:'Harsh',
+     caption:'this is a caption',
+     imageUrl:'https://mumbaimirror.indiatimes.com/photo/76922257.cms'},
+    { username:'Shivam',
+     caption:'I create Dope Shit' ,
+     imageUrl:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTgA0iOH9Rh9SX1hkqHYqKu4t6iHwvnbFsOVZCUuec6lQQRpzxd'}
+  ]);
   return (
     <div className="app">
      <div className="app__header">
        <img className='app__headerImage' src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/150px-Instagram_logo.svg.png" alt="" />
      </div>
-     <Post></Post>
-     <Post></Post>
-     <Post></Post>
-     <Post></Post>
+
+
+     {
+       posts.map((post)=> (
+         <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+       ))
+     } 
     </div>
   );
 }
--- instagram/src/Components/Post.jsx
+++ instagram/src/Components/Post.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import '../Css/Post.css'
 import Avatar from '@material-ui/core/Avatar';
-function Post() {
+function Post({username , caption , imageUrl }) {
     return (
         <div class='post'>
             <div className="post__header">
 
              <Avatar className='post__avatar' src="/static/images/avatar/1.jpg" alt='username' />
-            <h3>Username</h3>
+            <h3>{username}</h3>
             </div>
 
-            <img className='post__image' src="https://mumbaimirror.indiatimes.com/photo/76922257.cms" alt="Useername" />
+            <img className='post__image' src={imageUrl} alt="Useername" />
 
 
-            <h4 className='post__text'><strong>Username</strong>:Caption</h4>
+            <h4 className='post__text'><strong>{username}</strong>: {caption}</h4>
         </div>
     )
 }
