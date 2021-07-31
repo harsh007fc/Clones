58e63f1ffe377b7daf88562365a73b3b1cdf971b
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-08-01 01:55:09 +0530

    upto long url of spotify referenced from api of it

--- spotify/src/Components/Spotify.js
+++ spotify/src/Components/Spotify.js
@@ -2,7 +2,19 @@
 // documentation/web-playback-sdk/quick-start/#
 
 
-export const authEndpoint = "https://accounts.spotify.com/authorize";
+export const authEndpoint ="https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/"
+const clientId = "d56acb6de4d04e9ead8433fdb13df518";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+  ];
 
 
-const redirectUrl = "http://localhost:3000/"
+export const loginUrl =  `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+  )}&response_type=token&show_dialog=true`;
