1ea9d4bf6e24a47ae258f55d225cda14cf99376a
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-08-03 17:41:45 +0530

    upto logged in

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 import Login from './Components/Login';
+import { getTokenFromUrl } from './Components/Spotify';
 
 function App() {
+  const [token,setToken] = useState(null);
+
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if(_token){
+      setToken(_token)
+    }
+  }, [])
+
+
   return (
     <div className="app">
-      <Login/>
+      {
+        token ? (
+          <h1>I am Logged in</h1>
+        ): (
+          
+          <Login/>
+        )
+      }
     </div>
   );
 }
