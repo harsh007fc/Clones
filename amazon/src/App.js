2396021eb36caa1c94fb72bc3a81e61c20795d27
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-08-04 17:38:30 +0530

    added upto basci nav

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -1,11 +1,25 @@
-
+import React from 'react';
+import {BrowserRouter as Router , Switch , Route} from 'react-router-dom'
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div >
-      <h1>Hello</h1>
+    <Router>
+    <div className = 'app' >
+      <Switch>
+        <Route path='/checkout'>
+          <h1>Checkout 👍 </h1>
+        </Route>
+        <Route path='/login'>
+          <h1>Login ⚠ </h1>
+        </Route>
+        <Route path='/'>
+          <Header/>
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ amazon/src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import  '../Styles/Header.css'
+import { Link } from 'react-router-dom'
+
+function Header() {
+    return (
+        <nav className='header'>
+            <Link to='/'>
+            <img className='header__logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+            </Link>
+
+            <input type="text" className='header__searchInput'/>
+        </nav>
+    )
+}
+
+export default Header
