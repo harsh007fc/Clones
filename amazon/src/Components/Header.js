efb08b209836df7fd3e55fc8baad066ac1b9bacb
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-08-04 23:49:48 +0530

    added upto four links

--- amazon/src/Components/Header.js
+++ amazon/src/Components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import  '../Styles/Header.css'
 import { Link } from 'react-router-dom'
+import SearchIcon from '@material-ui/icons/Search'
 
 function Header() {
     return (
@@ -9,7 +10,38 @@ function Header() {
             <img className='header__logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
             </Link>
 
+            <div className="header__search">
             <input type="text" className='header__searchInput'/>
+            <SearchIcon className='header__searchIcon'></SearchIcon>
+            </div>
+
+
+            <div className="header__nav">
+                <Link to='/login' className='header__link'>
+                <div className="header__option">
+                <span className='header__optionLineOne'>Hello Username</span>
+                <span className='header__optionLineTwo'>Sign In</span>
+                </div>
+                </Link>
+                <Link to='/login' className='header__link'>
+                <div className="header__option">
+                <span className='header__optionLineOne'>Returns</span>
+                <span className='header__optionLineTwo'>& Orders</span>
+                </div>
+                </Link>
+                <Link to='/login' className='header__link'>
+                <div className="header__option">
+                <span className='header__optionLineOne'>Your</span>
+                <span className='header__optionLineTwo'>Prime</span>
+                </div>
+                </Link>
+                <Link to='/login' className='header__link'>
+                <div className="header__option">
+                <span className='header__optionLineOne'>Hello Username</span>
+                <span className='header__optionLineTwo'>Sign In</span>
+                </div>
+                </Link>
+            </div>
         </nav>
     )
 }
