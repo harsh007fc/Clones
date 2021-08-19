0ff8a547f8847c3764fd86f79cbfb33d61d772cd
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-08-19 22:41:33 +0530

    nav added

--- disney/src/App.js
+++ disney/src/App.js
@@ -1,10 +1,12 @@
 import {BrowserRouter as Router , Switch,Route} from 'react-router-dom'
 import './App.css';
 import Login from './Components/Login';
+import Header from './Components/Header';
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Switch>
           <Route>
             <Login />
